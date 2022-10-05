2d76c0c74c1702639f461f4b17933561691e48d7
Alejo <alejoczombos@gmail.com>
2022-10-05 12:28:48 -0300

    Disparos Primarios y secundarios

--- Metioritos/Juego/Naves/Player/Player.gd
+++ Metioritos/Juego/Naves/Player/Player.gd
@@ -1,17 +1,29 @@
+class_name Player
 extends RigidBody2D
 
 export var fuerzaMotor:int = 20
 export var fuerzaRotacion:int = 280
 
+onready var canion: Canion = $Canion
+onready var laser:RayoLaser = $LaserBeam2D
+
 var empuje:Vector2 = Vector2.ZERO
 var direccionRotacion:int = 0
 
-func _integrate_forces(state: Physics2DDirectBodyState) -> void:
+func _integrate_forces(_state: Physics2DDirectBodyState) -> void:
 	apply_central_impulse(empuje.rotated(rotation))
 	apply_torque_impulse(direccionRotacion * fuerzaRotacion)
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	player_input()
+
+func _unhandled_input(event: InputEvent) -> void:
+	# Disparo Rayo
+	if event.is_action_pressed("Disparo Secundario"):
+		laser.set_is_casting(true)
+	
+	if event.is_action_released("Disparo Secundario"):
+		laser.set_is_casting(false)
 
 func player_input() -> void:
 	#Empuje
@@ -27,3 +39,9 @@ func player_input() -> void:
 		direccionRotacion += 1
 	elif Input.is_action_pressed("RotarAntihorario"):
 		direccionRotacion -= 1
+	
+	#Disparo
+	if Input.is_action_pressed("Disparo Principal"):
+		canion.set_estaDisparando(true)
+	if Input.is_action_just_released("Disparo Principal"):
+		canion.set_estaDisparando(false)
