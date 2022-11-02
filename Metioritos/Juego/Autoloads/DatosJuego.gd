0afeb96bc60e44affa3d3af8309f6449e7e451c6
Alejo <alejoczombos@gmail.com>
2022-11-02 17:42:12 -0300

    Pulido

--- Metioritos/Juego/Autoloads/DatosJuego.gd
+++ Metioritos/Juego/Autoloads/DatosJuego.gd
@@ -7,3 +7,10 @@ func set_playerActual(player:Player) -> void:
 
 func get_playerActual() -> Player:
 	return playerActual
+
+func _ready():
+	Eventos.connect("naveDestruida", self, "_on_naveDestruida")
+
+func _on_naveDestruida(nave:Nave, _posicion, _explo) -> void:
+	if nave is Player:
+		playerActual = null
